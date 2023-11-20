5c46184849b53291b694d829ac5cdbbc9d549fd8
parzival <novak.a@protonmail.com>
2023-11-20 18:28:32 +0100

    004 relational operators

---
+++ 004_relational_operators/readme.md
@@ -0,0 +1,11 @@
+# Assignment (EN)
+
+Create accurate examples for each of the following relational operators and determine their truth value. 
+Output the results on the screen.
+
+- ==
+- !=
+- >
+- >=
+- <
+- <=
---
+++ 004_relational_operators/relational_operators.py
@@ -0,0 +1,10 @@
+#
+# Author: Adam Novak
+#
+
+print(3 > 2)     # True, because 3 is greater than 2
+print(3 >= 2)    # True, because 3 is greater than 2
+print(3 < 2)     # False, because 3 is greater than 2
+print(2 <= 3)    # True, because 2 is less than 3
+print(3 == 2)    # False, because 3 is not equal to 2
+print(3 != 2)    # True, because 3 is not equal to 2
