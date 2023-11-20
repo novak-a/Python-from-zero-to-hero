5ef2d8e91ded1a5aee26eb1616fd98503792a34f
parzival <novak.a@protonmail.com>
2023-11-20 18:00:16 +0100

    002 data types

---
+++ 002_data_types/data_types.py
@@ -0,0 +1,12 @@
+#
+# Author: Adam Novak
+#
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type("Programming"))       # String
+print(type([1, 2, 3, 4, 5]))     # List
+print(type({'key': 'value'}))    # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
---
+++ 002_data_types/readme.md
@@ -0,0 +1,13 @@
+# Assignment (EN)
+
+Generate correct examples for each of the following data types. 
+Verify their correctness using the type() function and display the results on the screen.
+
+- Integer
+- Float
+- Complex
+- String
+- List
+- Dictionary
+- Set
+- Tuple
