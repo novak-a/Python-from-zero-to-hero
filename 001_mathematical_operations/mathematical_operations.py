e701646f0b74f3da034c7e4f6925f7b37f0a6877
parzival <novak.a@protonmail.com>
2023-11-20 17:46:09 +0100

    001 mathematical operations

---
+++ 001_mathematical_operations/mathematical_operations.py
@@ -0,0 +1,11 @@
+#
+# Author: Adam Novak
+#
+
+print(3 + 2)   # addition (+)
+print(3 - 3)   # subtraction (-)
+print(3 * 2)   # multiplication (*)
+print(3 / 2)   # division (/)
+print(3 // 2)  # Floor division operator (//)
+print(3 ** 2)  # exponential (**)
+print(3 % 2)   # modulus (%)
---
+++ 001_mathematical_operations/readme.md
@@ -0,0 +1,4 @@
+# Assignment (EN)
+
+Perform various operations using the integers 3 and 2. Output the results on the screen for the following operations: 
+addition, subtraction, multiplication, division, exponential, modulus, and integer division.
